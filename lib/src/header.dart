62e345caad84dc331d85d04b550a6a66698029e9
xuantung <xuantung4195@gmail.com>
2022-11-30 09:02:08 +0700

    Fix confirm button size

--- lib/src/header.dart
+++ lib/src/header.dart
@@ -64,141 +64,130 @@ class _HeaderState extends State<Header> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      child: Stack(
+        alignment: Alignment.centerRight,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 6),
-            child: IconButton(
-                icon: widget.decoration!.cancelIcon ??
-                    Icon(Icons.arrow_back_outlined),
-                onPressed: () {
-                  if (_arrowAnimation.value == 1)
-                    _arrowAnimController!.reverse();
-                  widget.onBack();
-                }),
-          ),
-          Expanded(
-            flex: 3,
-            child: Center(
-              child: JumpingButton(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedSwitcher(
-                      duration: Duration(milliseconds: 200),
-                      transitionBuilder:
-                          (Widget child, Animation<double> animation) {
-                        return SlideTransition(
-                          child: child,
-                          position: Tween<Offset>(
-                                  begin: Offset(0.0, -0.5),
-                                  end: Offset(0.0, 0.0))
-                              .animate(animation),
-                        );
-                      },
-                      child: Text(
-                        widget.getAlbumName == null ? widget.selectedAlbum.entity.name : widget.getAlbumName!.call(widget.selectedAlbum.entity.name),
-                        style: widget.decoration!.albumTitleStyle,
-                        key: ValueKey<String>(widget.selectedAlbum.id),
-                      ),
-                    ),
-                    AnimatedBuilder(
-                      animation: _arrowAnimation,
-                      builder: (context, child) => Transform.rotate(
-                        angle: _arrowAnimation.value * pi,
-                        child: Icon(
-                          Icons.keyboard_arrow_up_outlined,
-                          size: (widget
-                                      .decoration!.albumTitleStyle?.fontSize) !=
-                                  null
-                              ? widget.decoration!.albumTitleStyle!.fontSize! *
-                                  1.5
-                              : 20,
-                          color: widget.decoration!.albumTitleStyle?.color ??
-                              Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  if (widget.albumController.isPanelOpen) {
-                    widget.albumController.close();
-                    _arrowAnimController!.reverse();
-                  }
-                  if (widget.albumController.isPanelClosed) {
-                    widget.albumController.open();
-                    _arrowAnimController!.forward();
-                  }
-                },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6),
+                child: IconButton(
+                    icon: widget.decoration!.cancelIcon ?? Icon(Icons.arrow_back_outlined),
+                    onPressed: () {
+                      if (_arrowAnimation.value == 1) _arrowAnimController!.reverse();
+                      widget.onBack();
+                    }),
               ),
-            ),
+              Expanded(
+                flex: 9,
+                child: Center(
+                  child: JumpingButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        AnimatedSwitcher(
+                          duration: Duration(milliseconds: 200),
+                          transitionBuilder: (Widget child, Animation<double> animation) {
+                            return SlideTransition(
+                              child: child,
+                              position: Tween<Offset>(begin: Offset(0.0, -0.5), end: Offset(0.0, 0.0)).animate(animation),
+                            );
+                          },
+                          child: Text(
+                            widget.getAlbumName == null ? widget.selectedAlbum.entity.name : widget.getAlbumName!.call(widget.selectedAlbum.entity.name),
+                            style: widget.decoration!.albumTitleStyle,
+                            key: ValueKey<String>(widget.selectedAlbum.id),
+                          ),
+                        ),
+                        AnimatedBuilder(
+                          animation: _arrowAnimation,
+                          builder: (context, child) => Transform.rotate(
+                            angle: _arrowAnimation.value * pi,
+                            child: Icon(
+                              Icons.keyboard_arrow_up_outlined,
+                              size: (widget.decoration!.albumTitleStyle?.fontSize) != null ? widget.decoration!.albumTitleStyle!.fontSize! * 1.5 : 20,
+                              color: widget.decoration!.albumTitleStyle?.color ?? Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      if (widget.albumController.isPanelOpen) {
+                        widget.albumController.close();
+                        _arrowAnimController!.reverse();
+                      }
+                      if (widget.albumController.isPanelClosed) {
+                        widget.albumController.open();
+                        _arrowAnimController!.forward();
+                      }
+                    },
+                  ),
+                ),
+              ),
+              if (widget.mediaCount == MediaCount.multiple)
+                Expanded(
+                  flex: 3,
+                  child: SizedBox(),
+                ),
+            ],
           ),
           if (widget.mediaCount == MediaCount.multiple)
-            Expanded(
-              flex: 1,
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 100),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return SlideTransition(
-                    child: child,
-                    position: Tween<Offset>(
-                            begin: Offset(1, 0.0), end: Offset(0.0, 0.0))
-                        .animate(animation),
-                  );
-                },
-                child: (selectedMedia.length > 0)
-                    ? TextButton(
-                        key: Key('button'),
+            AnimatedSwitcher(
+              duration: Duration(milliseconds: 100),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return SlideTransition(
+                  child: child,
+                  position: Tween<Offset>(begin: Offset(1, 0.0), end: Offset(0.0, 0.0)).animate(animation),
+                );
+              },
+              child: (selectedMedia.length > 0)
+                  ? Padding(
+                      key: Key('button'),
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: TextButton(
                         child: Row(
                           mainAxisSize: MainAxisSize.min,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
                               widget.decoration!.completeText,
-                              style: widget.decoration!.completeTextStyle ??
-                                  TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500),
+                              style: widget.decoration!.completeTextStyle ?? TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
                             ),
                             Text(
                               ' (${selectedMedia.length})',
                               style: TextStyle(
-                                color: widget
-                                        .decoration!.completeTextStyle?.color ??
-                                    Colors.white,
-                                fontSize: widget.decoration!.completeTextStyle
-                                            ?.fontSize !=
-                                        null
-                                    ? widget.decoration!.completeTextStyle!
-                                            .fontSize! *
-                                        0.77
-                                    : 11,
+                                color: widget.decoration!.completeTextStyle?.color ?? Colors.white,
+                                fontSize: widget.decoration!.completeTextStyle?.fontSize != null ? widget.decoration!.completeTextStyle!.fontSize! * 0.77 : 11,
                                 fontWeight: FontWeight.w300,
                               ),
                             ),
                           ],
                         ),
-                        onPressed: selectedMedia.length > 0
-                            ? () => widget.onDone(selectedMedia)
-                            : null,
+                        onPressed: selectedMedia.length > 0 ? () => widget.onDone(selectedMedia) : null,
                         style: widget.decoration!.completeButtonStyle ??
                             ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                  Theme.of(context).primaryColor),
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(3))),
+                              backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
+                              shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(3))),
                             ),
-                      )
-                    : Container(
-                        key: Key('blank'),
                       ),
-              ),
-            ),
+                    )
+                  : Container(
+                      key: Key('blank'),
+                      child: Opacity(
+                        opacity: 0,
+                        child: TextButton(
+                          child: Text(
+                            'Done',
+                            style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+            )
         ],
       ),
     );
